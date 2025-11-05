1438bfd40180418b87cc867a84d4a9027e152e32
dante <knownasdante@gmail.com>
2025-11-05 16:13:34 +0100

    feat: dynamic stylesheets injection

--- app/scripts/loadContainers.js
+++ app/scripts/loadContainers.js
@@ -1,19 +1,21 @@
 import * as element from "../scripts/modules/element.js"
 
-export const containers = (box) => {
-    const mainBox = element.add(box, "section", "mainBox", "mainBox relative max")
-    const componentBox = element.add(mainBox, "div", "componentBox", "componentBox relative max")
-    const listMenuBox = element.add(box, "section", "listMenuBox", "listMenuBox absolute maxV")
-    const confMenuBox = element.add(box, "section", "confMenuBox", "confMenuBox absolute maxV")
-    const controlsBox = element.add(box, "section", "controlsBox", "controlsBox absolute")
-    const infoBox = element.add(box, "section", "infoBox", "infoBox absolute")
+export const containers = (box) => {    /* only main style class */
+    const mainBox = element.add(box, "section", "mainBox", "mainBox")
+    const componentBox = element.add(mainBox, "div", "componentBox", "componentBox")
+    const listMenuBox = element.add(box, "section", "listMenuBox", "listMenuBox panelMenu")
+    const confMenuBox = element.add(box, "section", "confMenuBox", "confMenuBox panelMenu")
+    const controlsBox = element.add(box, "section", "controlsBox", "controlsBox")
+    const infoBox = element.add(box, "section", "infoBox", "infoBox")
 }
 
 export const addStyles = () => {
     const path = [
-        "app/styles/globalClass.css",
-        "app/styles/mainContainers.css"
+        { rel: "shortcut icon", type: "image/x-icon", href: "app/assets/images/icons/logo_128.png" },
+        { rel: "stylesheet", href: "app/styles/globalConf.css" },
+        { rel: "stylesheet", href: "app/styles/globalClass.css" },
+        { rel: "stylesheet", href: "app/styles/mainContainers.css" }
     ]
 
-    path.forEach(url => element.add(document.head, "link", null, "mainContainers", { href: url, rel: "stylesheet" }))
+    Object.entries(path).forEach(item => element.add(document.head, "link", null, "mainContainers", item[1]))
 }
